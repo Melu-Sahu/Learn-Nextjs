20ec1a0fb874fd4e737c325c4a536d88fde30b8a
Melu Sahu <94543371+Melu-Sahu@users.noreply.github.com>
2025-09-01 06:33:13 +0000

    database connection added

---
+++ mistrymsg/src/lib/dbConnect.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/mistrymsg";
+
+
+async function dbConnect(): Promise<void> {
+
+    if (connection.isConnected) {
+        console.log("Already connected to database");
+        return;
+    }
+
+    try {
+        const  db_string = await mongoose.connect(MONGODB_URI, {});
+        console.log(db_string);
+    connection.isConnected = db_string.connections[0].readyState;
+        console.log("Connected to database");
+        
+    } catch (error) {
+        console.error("Error connecting to database:", error);
+        process.exit(1);
+    }
+}
+
+
+
+
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
